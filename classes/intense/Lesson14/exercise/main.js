95cb2e69c5ec9939931776c75f761b6561af9a5f
Bayasgalan Battulga <b.bayasgalan0912@gmail.com>
2023-06-27 10:13:10 +0800

    update

--- classes/intense/Lesson14/exercise/main.js
+++ classes/intense/Lesson14/exercise/main.js
@@ -13,5 +13,28 @@ console.log("Sum=" + sum)
 
 
 
+// function findMaxNumber(arr) {
+//     if (arr.length === 0) {
+//         return null; // Return null if the array is empty
+//     }
+
+//     let max = arr[0]; // Assume the first element is the maximum
+
+//     for (let i = 1; i < arr.length; i++) {
+//         if (arr[i] > max) {
+//             max = arr[i]; // Update max if a larger element is found
+//         }
+//     }
+
+//     return max; // Return the maximum number
+// }
+
+// // Example usage
+// const numbers = [3, 7, 2, 9, 4, 1];
+// const maxNumber = findMaxNumber(numbers);
+// console.log(maxNumber); // Output: 9
+
+
+
 
 
