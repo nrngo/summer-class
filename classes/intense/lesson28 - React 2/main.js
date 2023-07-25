6ca5730a6d562e8b5ba456d705c240e57f9bcee8
Bayasgalan Battulga <b.bayasgalan0912@gmail.com>
2023-07-25 10:44:07 +0800

    update

---
+++ classes/intense/lesson28 - React 2/main.js
@@ -0,0 +1,25 @@
+const car = {
+    brand: 'Toyota',
+    name: 'Prius',
+    price: 2000000,
+    stop: () => { },
+    move: () => { },
+}
+// const brand = car.brand
+// const name = car.name
+// const price = car.price
+const { price } = car
+console.log(price)
+// const sum = (a, b) => {
+//     return a + b
+// }
+// sum(100, 200)
+
+// const cars = ['prius', 'crown', 'land']
+// cars.map((element) => {
+//     console.log("element", element)
+// })
+// const numbers = [1, 2, 3, 4, 5, 6]
+// let total = 0;
+
+// console.log("numbers.length", numbers.length)
