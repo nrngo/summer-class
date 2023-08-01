bb97cee3f402372d560dfe3b9d372671f1f7fdb7
Bayasgalan Battulga <b.bayasgalan0912@gmail.com>
2023-08-01 09:29:17 +0800

    update

---
+++ classes/intense/lesson33 - React 7 - Example project/user-list.js
@@ -0,0 +1,172 @@
+const users = [
+    {
+        "image": "https://randomuser.me/api/portraits/men/1.jpg",
+        "address": {
+            "city": "kilcoole",
+            "street": "new road",
+            "number": 7682,
+            "zipcode": "12926-3874"
+        },
+        "id": 1,
+        "email": "john@gmail.com",
+        "name": {
+            "firstname": "john",
+            "lastname": "doe"
+        },
+        "phone": "1-570-236-7033",
+
+    },
+    {
+        "image": "https://randomuser.me/api/portraits/men/2.jpg",
+        "address": {
+            "city": "kilcoole",
+            "street": "Lovers Ln",
+            "number": 7267,
+            "zipcode": "12926-3874"
+        },
+        "id": 2,
+        "email": "morrison@gmail.com",
+        "name": {
+            "firstname": "david",
+            "lastname": "morrison"
+        },
+        "phone": "1-570-236-7033",
+
+    },
+    {
+        "image": "https://randomuser.me/api/portraits/men/3.jpg",
+        "address": {
+            "city": "Cullman",
+            "street": "Frances Ct",
+            "number": 86,
+            "zipcode": "29567-1452"
+        },
+        "id": 3,
+        "email": "kevin@gmail.com",
+        "name": {
+            "firstname": "kevin",
+            "lastname": "ryan"
+        },
+        "phone": "1-567-094-1345",
+
+    },
+    {
+        "image": "https://randomuser.me/api/portraits/men/4.jpg",
+        "address": {
+            "city": "San Antonio",
+            "street": "Hunters Creek Dr",
+            "number": 6454,
+            "zipcode": "98234-1734"
+        },
+        "id": 4,
+        "email": "don@gmail.com",
+        "name": {
+            "firstname": "don",
+            "lastname": "romer"
+        },
+        "phone": "1-765-789-6734",
+
+    },
+    {
+        "image": "https://randomuser.me/api/portraits/men/5.jpg",
+        "address": {
+            "city": "san Antonio",
+            "street": "adams St",
+            "number": 245,
+            "zipcode": "80796-1234"
+        },
+        "id": 5,
+        "email": "derek@gmail.com",
+        "name": {
+            "firstname": "derek",
+            "lastname": "powell"
+        },
+        "phone": "1-956-001-1945",
+
+    },
+    {
+        "image": "https://randomuser.me/api/portraits/men/6.jpg",
+        "address": {
+            "city": "el paso",
+            "street": "prospect st",
+            "number": 124,
+            "zipcode": "12346-0456"
+        },
+        "id": 6,
+        "email": "david_r@gmail.com",
+        "name": {
+            "firstname": "david",
+            "lastname": "russell"
+        },
+        "phone": "1-678-345-9856",
+
+    },
+    {
+        "image": "https://randomuser.me/api/portraits/men/7.jpg",
+        "address": {
+            "city": "fresno",
+            "street": "saddle st",
+            "number": 1342,
+            "zipcode": "96378-0245"
+        },
+        "id": 7,
+        "email": "miriam@gmail.com",
+        "name": {
+            "firstname": "miriam",
+            "lastname": "snyder"
+        },
+        "phone": "1-123-943-0563",
+
+    },
+    {
+        "image": "https://randomuser.me/api/portraits/men/8.jpg",
+        "address": {
+            "city": "mesa",
+            "street": "vally view ln",
+            "number": 1342,
+            "zipcode": "96378-0245"
+        },
+        "id": 8,
+        "email": "william@gmail.com",
+        "name": {
+            "firstname": "william",
+            "lastname": "hopkins"
+        },
+        "phone": "1-478-001-0890",
+
+    },
+    {
+        "image": "https://randomuser.me/api/portraits/women/9.jpg",
+        "address": {
+            "city": "miami",
+            "street": "avondale ave",
+            "number": 345,
+            "zipcode": "96378-0245"
+        },
+        "id": 9,
+        "email": "kate@gmail.com",
+        "name": {
+            "firstname": "kate",
+            "lastname": "hale"
+        },
+        "phone": "1-678-456-1934",
+
+    },
+    {
+        "image": "https://randomuser.me/api/portraits/men/10.jpg",
+        "address": {
+            "city": "fort wayne",
+            "street": "oak lawn ave",
+            "number": 526,
+            "zipcode": "10256-4532"
+        },
+        "id": 10,
+        "email": "jimmie@gmail.com",
+        "name": {
+            "firstname": "jimmie",
+            "lastname": "klein"
+        },
+        "phone": "1-104-001-4567",
+
+    }
+]
