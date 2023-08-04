a4b91852b6d77a0fb83f649b1d5aaaa9d0d6acda
Bayasgalan Battulga <b.bayasgalan0912@gmail.com>
2023-08-04 12:22:29 +0800

    update

---
+++ classes/beginner/lesson16 JS array, массив/dasgal.js
@@ -0,0 +1,6 @@
+let numbers = [1, 2, 3, 4, 5]
+let chars = ['a', 'b', 'c', 'd', 'e']
+let object_array = [{ name: '' }, { name: '' }, { name: '' }]
+let boolean_array = [true, false, true, false]
+
+// push , unshift
