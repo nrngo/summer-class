76f3d489317389f87b89609e53d19a008f85c0f9
Bayasgalan Battulga <b.bayasgalan0912@gmail.com>
2023-08-08 13:23:12 +0800

    update

--- classes/intermediate/react-app-demo/src/App.js
+++ classes/intermediate/react-app-demo/src/App.js
@@ -1,8 +1,5 @@
 import { useState } from 'react';
 import './App.css';
-import Child1 from './components/Child1';
-import Child2 from './components/Child2';
-import TsagAgaar from './components/TsagAgaar';
 import AntDemo from './components/AntDemo';
 function App() {
   const [number, setNumber] = useState(0);
