ea5b0e4d0ec887cb6ee7ea7dd4b959fe63ba5563
Bayasgalan Battulga <b.bayasgalan0912@gmail.com>
2023-08-08 14:34:20 +0800

    update

--- classes/beginner/lesson18/app.js
+++ classes/beginner/lesson18/app.js
@@ -1,25 +1,45 @@
+// 1. Үгийн жагсаалтаа үүсгэнэ
 const words = [
     {
         word: "Apple",
-        hint: "Fruit ...",
+        hint: "Бөөрөнхий хэлбэртэй амттай жимс",
     },
     {
-        word: "Apple",
-        hint: "Fruit ...",
+        word: "pineapple",
+        hint: "Боргоцой + Алим",
     },
     {
-        word: "Apple",
-        hint: "Fruit ...",
-    }
+        word: "jump",
+        hint: "Гэнэт дээшлэх",
+    },
+
 ]
-// 1. Үгийн жагсаалтаа үүсгэнэ
 // 2. Санамсаргүй байдлаар жагсаалтаас сонгоно
+// 0 - 2 
+const randomIndex = Math.floor(Math.random() * 3)
+const randomWord = words[randomIndex]
+
+console.log("randomWord", randomWord)
+
+// hariu.innerHTML
+
 // 3. Санамсаргүй сонгосон үгийн урттай тэнцүү
 // _ зураас харуулна , Хинт харуулна
+const displayWord = document.getElementById("displayWord")
+console.log('displayWord', displayWord, randomWord)
+for (let i = 0; i < randomWord.word.length; i++) {
+    displayWord.append("_")
+}
+const hint = document.getElementById("hint")
+hint.textContent = randomWord.hint
 // 4. Товч дарах үед функц ажиллана.
 // Функц нь Санамсаргүй сонгосон үг
 //  дотор дарсан үсэн
 // байна уу шалгана
+const usegShalga = (useg) => {
+    console.log("usegShalga ajillaj bn==>", useg)
+}
+
 // 5. Үсэг таасан бол орсон бол тухайн үсгийг харуулна
 // 6. Үсэг таагаагүй бол алдааг нь тоолно
 // 7. Алдаа 5-с хэтэрсэн бол Хожигдоно
